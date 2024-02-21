edb24bb64aae3e8d671d54042a175174b85b3ee2
Leandro <leandrovilla07@gmail.com>
2024-02-21 12:52:07 +0100

    Ejercicio 10 resuelto

---
+++ L0.1_Ejercicios/src/ejercicio10.c
@@ -0,0 +1,24 @@
+/*Escriba un programa en C que calcule el factorial de un número no negativo, n, 
+mediante multiplicaciones iterativas. Aunque debe probar el correcto funcionamiento 
+de su programa para múltiples valores de n, la entrega final del mismo debe ser para n = 7. 
+El programa debe imprimir el valor del factorial(n) al final del programa. n debe ser una 
+variable definida estáticamente dentro del programa. Guarde el programa como Factorial.c*/
+
+#include <stdio.h>
+
+#define N 7
+
+__uint32_t factorial(__uint32_t num);
+
+int main(void) 
+{
+    __uint32_t resultado = factorial(N);
+    printfNexys("Resultado: %d\n", resultado);
+    return 0;
+}
+
+__uint32_t factorial(__uint32_t num){
+    if (num == 0 || num == 1) return 1;
+    else if (num > 1) return num*(factorial(num-1));
+    else return 0;
+}
